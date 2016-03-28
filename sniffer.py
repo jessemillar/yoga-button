2b0ab3051296555c7aba38dc6bf4bf4ead87ece9
Jesse Millar <hellojessemillar@gmail.com>
2016-03-28 17:20:36 -0600

    Making the sniffer smarter with CEC

--- sniffer.py
+++ sniffer.py
@@ -1,6 +1,5 @@
 """Sniff the network for our Dash Button's ARP request and then start a yoga video"""
 import os
-import subprocess
 
 from omxplayer import OMXPlayer
 from scapy.all import *
@@ -8,13 +7,18 @@ from scapy.all import *
 
 def arp_display(pkt):
     """Where the magic happens"""
-    if pkt[ARP].op == 1:  # who-has (request)
-        if pkt[ARP].psrc == '0.0.0.0':  # ARP Probe
-            if pkt[ARP].hwsrc == '74:c2:46:9a:ce:38':  # Smart Water
+    if pkt[ARP].op == 1: # who-has (request)
+        if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+            if pkt[ARP].hwsrc == '74:c2:46:9a:ce:38': # Smart Water
                 print "Pushed Yoga Button"
-                player.play_pause()
+                os.system("echo 'on 0' | cec-client -s") # Turn on the TV
+
+                if player.is_playing():
+                    player.quit()
+                else:
+                    player.play()
             else:
                 print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
-player = OMXPlayer('videos/yoga.mp4')
-print sniff(prn=arp_display, filter="arp", store=0, count=0)  # count=0 means to sniff forever
+player = OMXPlayer('videos/yoga.m4a')
+print sniff(prn=arp_display, filter="arp", store=0, count=0) # count=0 means to sniff forever
